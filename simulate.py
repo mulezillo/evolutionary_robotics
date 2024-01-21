2ce0ffb892d8722ca0c8a37a7dbfe31ad55eaff4
mulezillo <85587339+mulezillo@users.noreply.github.com>
2024-01-21 17:36:20 -0500

    Update simulate.py
    assignmnet 1 soluation

--- simulate.py
+++ simulate.py
@@ -1,1 +1,11 @@
+import pybullet as p
+import time
 
+
+if __name__ == "__main__":
+    physicsClient = p.connect(p.GUI)
+    for i in range(1000):
+        p.stepSimulation()
+        time.sleep(0.06)
+        print(i)
+    p.disconnect()
