90c821f465c5fc6e8921bca68946d195ba5f5f44
mulezillo <85587339+mulezillo@users.noreply.github.com>
2024-01-21 19:27:22 -0500

    Update simulate.py
    adding multiple boxes

--- simulate.py
+++ simulate.py
@@ -1,10 +1,18 @@
 import pybullet as p
+import pybullet_data
 import time
 
 
 if __name__ == "__main__":
     physicsClient = p.connect(p.GUI)
-    p.loadSDF("box.sdf")
+
+    # add path to URDF files
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.setGravity(0, 0, -9.8)
+
+    # add a "floor"
+    planeId = p.loadURDF("plane.urdf")
+    p.loadSDF("boxes.sdf")
     for i in range(1000):
         p.stepSimulation()
         time.sleep(0.06)
