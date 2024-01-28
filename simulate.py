99d70f74d9b0dfae1ca8d5aff8750ea62ffe317b
mulezillo <85587339+mulezillo@users.noreply.github.com>
2024-01-28 17:02:55 -0500

    Update simulate.py
    adding sensors

--- simulate.py
+++ simulate.py
@@ -1,7 +1,13 @@
 import pybullet as p
 import pybullet_data
 import time
+import pyrosim.pyrosim as pyrosim
+import numpy as np
 
+
+num_sims = 100
+back_leg_file = "data/back_leg_sensor_values.npy"
+front_leg_file = "data/front_leg_sensor_values.npy"
 
 if __name__ == "__main__":
     physicsClient = p.connect(p.GUI)
@@ -12,10 +18,21 @@ if __name__ == "__main__":
 
     # add a "floor"
     planeId = p.loadURDF("plane.urdf")
-    robitId = p.loadURDF("body.urdf")
+    # add robot
+    robotId = p.loadURDF("body.urdf")
+    # note that we could load this robot multiple times, create multiple IDs, and add multiple identical robots
+    pyrosim.Prepare_To_Simulate(robotId)
     p.loadSDF("world.sdf")
-    for i in range(1000):
+    backLegSensorValues = np.zeros(num_sims)
+    frontLegSensorValues = np.zeros(num_sims)
+    for i in range(num_sims):
         p.stepSimulation()
+        # add touch sensor
+        # Note: touch sensors only work for non-root links
+        backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+        frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
         time.sleep(0.06)
-        print(i)
     p.disconnect()
+    print(f"backLegSensorValues: {backLegSensorValues}")
+    np.save(back_leg_file, backLegSensorValues)
+    np.save(front_leg_file, frontLegSensorValues)
